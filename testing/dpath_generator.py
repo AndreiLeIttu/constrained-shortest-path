9b19e5f9e0e6b798ee0c7e28df10f27ba1823c43
andreileittu <andrei.ionita2003@gmail.com>
2023-11-14 20:19:21 +0100

    Implemented a working test generator in python that writes the output to a new .dzn file that will be used by minizinc as an input.

--- testing/dpath_generator.py
+++ testing/dpath_generator.py
@@ -2,7 +2,7 @@ import numpy as np
 import random
 
 nodes = random.randint(1,100)
-edges = random.randint(1,1000)
+edges = random.randint(1,min((nodes-1)*nodes/2,100))
 source = random.randint(1,nodes)
 target = random.randint(1,nodes)
 while(target==source):
@@ -10,13 +10,40 @@ while(target==source):
 
 #constraint sets generation
 numConstraintSets = random.randint(0,10)
-constraintSets=[]
-for i in numConstraintSets:
+constraintSets = []
+constrString = "["
+
+for i in range(numConstraintSets):
+    constrString+="{"
     size = random.randint(1,10)
-    arr = np.randint(1,nodes,size)
+    arr = np.random.randint(1,nodes,size)
     constraintSets.append(arr)
+    for num in range(len(arr)-1):
+        constrString+=str(num)+','
+    constrString+=str(arr[len(arr)-1])
+    constrString+="},"
 
+constrString=constrString[:-1]
+constrString += "]"
 
+#edges generation
+toNodes = []
+fromNodes = []
+weights = []
 
+for i in range(edges):
+    weight = random.randint(0,100)
+    x = random.randint(1,nodes)
+    y = random.randint(1,nodes)
+    while (x==y):
+        y=random.randint(1,nodes)
+    toNodes.append(x)
+    fromNodes.append(y)
+    weights.append(weight)
 
+#printing to another file 
+f = open("testing/generatedTest.dzn", "w")
+output = ""
+output += f"N={nodes};\nE={edges};\nC={numConstraintSets};\nsource={source};\ntarget={target};\nfrom={fromNodes};\nto={toNodes};\nweights={weights};\nv_constraint="+constrString
+f.write(output)
 
