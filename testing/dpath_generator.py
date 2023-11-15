90255224c1d767de36a1a5f29235e290d030c12a
andreileittu <andrei.ionita2003@gmail.com>
2023-11-15 22:13:17 +0100

    Fixed case with no constraints given.

--- testing/dpath_generator.py
+++ testing/dpath_generator.py
@@ -16,14 +16,15 @@ constrString = "["
 for i in range(numConstraintSets):
     constrString+="{"
     size = random.randint(1,10)
+    print(size)
     arr = np.random.randint(1,nodes,size)
     constraintSets.append(arr)
     for num in range(len(arr)-1):
-        constrString+=str(num)+','
+        constrString+=str(arr[num])+','
     constrString+=str(arr[len(arr)-1])
     constrString+="},"
-
-constrString=constrString[:-1]
+if  numConstraintSets!=0:
+    constrString=constrString[:-1]
 constrString += "]"
 
 #edges generation
@@ -42,7 +43,7 @@ for i in range(edges):
     weights.append(weight)
 
 #printing to another file 
-f = open("testing/generatedTest.dzn", "w")
+f = open("./generatedTest.dzn", "w")
 output = ""
 output += f"N={nodes};\nE={edges};\nC={numConstraintSets};\nsource={source};\ntarget={target};\nfrom={fromNodes};\nto={toNodes};\nweights={weights};\nv_constraint="+constrString
 f.write(output)
