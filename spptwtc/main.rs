838909bffb45dcb4fee8936755bf2f5ac23262ed
AndreiLeIttu <andrei.ionita2003@gmail.com>
2024-06-06 14:26:56 +0200

    Added preprocessing of time windows.

--- spptwtc/main.rs
+++ spptwtc/main.rs
@@ -1,19 +1,20 @@
 use std::env;
+use std::fs;
 use std::collections::VecDeque;
 
 fn main() {
-    let args: Vec<usize> = env::args()
-        .skip(1)
-        .map(|arg| arg.parse().expect("Argument is not of type usize"))
+    let pre_args: Vec<String> = env::args().collect();
+    let file_path = &pre_args[1];
+    let input = fs::read_to_string(file_path).expect("Failed to read input file");
+    let args: Vec<usize> = input.split_whitespace()
+        .map(|s| s.parse().expect("Failed to parse number"))
         .collect();
 
     let nodes: usize = args[0];
     let edges: usize = args[1];
 
-    println!("{}", nodes);
-
     let mut index = 2;
-    let mut neigh: Vec<Vec<usize>> = vec![vec![]; nodes + 1];
+    let mut neigh: Vec<Vec<(usize, usize)>> = vec![vec![]; nodes + 1];
     let mut pre: Vec<Vec<(usize, usize, usize)>> = vec![vec![]; nodes + 2];
     let mut deg: Vec<usize> = vec![0; nodes + 2];
     
@@ -24,7 +25,7 @@ fn main() {
         let cost: usize = args[index + 3];
         index += 4;
 
-        neigh[a].push(b);
+        neigh[a].push((b,t));
         pre[b].push((a, t, cost));
         //not source
         if a != 0 {
@@ -61,7 +62,7 @@ fn main() {
     while let Some(x) = queue.pop_front() {
         ordering[cnt] = x;
         cnt += 1;
-        for &neighbor in &neigh[x] {
+        for &(neighbor, _) in &neigh[x] {
             deg[neighbor] -= 1;
             if deg[neighbor] == 0 && neighbor != nodes+1 {
                 queue.push_back(neighbor);
@@ -69,8 +70,35 @@ fn main() {
         }
     }
 
+    //constraining the start of the time windows
+    for i in 0..nodes {
+        let node = ordering[i];
+        let mut min_start = 1_000_000_000;
+        for &(prev, t, _) in &pre[node] {
+            min_start = min_start.min(time_w[prev].0 + t);
+        }
+        time_w[node].0 = time_w[node].0.max(min_start)
+    }
+
+    //constraining the end of the time windows
+    for i in (0..nodes).rev() {
+        let node = ordering[i];
+        let mut max_end = 0;
+        for &(next, t) in &neigh[node] {
+            if next != nodes+1 {
+                max_end = max_end.max(time_w[next].1-t);
+            }
+        }
+        if max_end != 0 {
+            time_w[node].1 = time_w[node].1.min(max_end);
+        }
+    }
+
+    // println!("{:?}", time_w);
+
     //dp initialization
     let mut ans: Vec<Vec<usize>> = vec![vec![1_000_000_000; 0]; nodes + 1];
+    let mut res = 1_000_000_000;
 
     for i in 0..=nodes {
         let size = time_w[i].1 - time_w[i].0 + 1;
@@ -79,31 +107,31 @@ fn main() {
     ans[0][0] = 0;
 
     //dp
-    for i in 0..nodes {
-        let node = ordering[i];
+    for i in 0..=nodes {
+        let node;
+        if i<nodes {
+            node = ordering[i];
+        }
+        else {
+            node = nodes+1;
+        }
         for &(prev, t, cost) in &pre[node] {
             for itr in time_w[prev].0..=time_w[prev].1 {
                 if ans[prev][itr - time_w[prev].0] != 1_000_000_000 {
                     let arrival = itr + t;
-                    if arrival <= time_w[node].1 {
-                        let index = (arrival - time_w[node].0).max(0);
-                        ans[node][index] = ans[node][index]
-                            .min(ans[prev][itr - time_w[prev].0] + cost + (arrival - time_w[node].0).max(0) * node_costs[node]);
-                        //println!("ans[{}][{}] = {}", node, prev, ans[node][index]);
-                    }
+                    if node != nodes+1 {
+                        if arrival <= time_w[node].1 {
+                            let index = (arrival - time_w[node].0).max(0);
+                            ans[node][index] = ans[node][index]
+                                .min(ans[prev][itr - time_w[prev].0] + cost + (arrival - time_w[node].0).max(0) * node_costs[node]);
+                            //println!("ans[{}][{}] = {}", node, prev, ans[node][index]);
+                        }
+                    }  
+                    else { //sink node cost computation
+                        //println!("arrival: {} from {}", arrival, prev);
+                        res = res.min(ans[prev][itr - time_w[prev].0] + cost + arrival * node_costs[nodes + 1]);
+                    } 
                 }
-            }
-        }
-    }
-
-    //sink node final node computation
-    let mut res = 1_000_000_000;
-    for &(prev, t, cost) in &pre[nodes + 1] {
-        for itr in time_w[prev].0..=time_w[prev].1 {
-            if ans[prev][itr - time_w[prev].0] != 1_000_000_000 {
-                let arrival = itr + t;
-                println!("arrival: {} from {}", arrival, prev);
-                res = res.min(ans[prev][itr - time_w[prev].0] + cost + arrival * node_costs[nodes + 1]);
             }
         }
     }
