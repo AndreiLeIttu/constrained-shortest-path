7ca4d60d10010a1e77cae34371921415601bea57
AndreiLeIttu <andrei.ionita2003@gmail.com>
2025-05-27 17:52:57 +0200

    Adapt dp algorithm to print best path

--- spptwtc/main.rs
+++ spptwtc/main.rs
@@ -106,35 +106,71 @@ fn main() {
     }
     ans[0][0] = 0;
 
+    let mut parent: Vec<Vec<Option<(usize, usize)>>> = vec![vec![None; 0]; nodes + 2];
+    for i in 0..=nodes+1 {
+        let size = time_w[i].1 - time_w[i].0 + 1;
+        parent[i] = vec![None; size];
+    }
+
+    let mut final_arrival_time = 1_000_000_000;
+
     //dp
     for i in 0..=nodes {
-        let node;
-        if i<nodes {
-            node = ordering[i];
-        }
-        else {
-            node = nodes+1;
-        }
+        let node = if i < nodes { ordering[i] } else { nodes + 1 };
+
         for &(prev, t, cost) in &pre[node] {
             for itr in time_w[prev].0..=time_w[prev].1 {
-                if ans[prev][itr - time_w[prev].0] != 1_000_000_000 {
+                let prev_index = itr - time_w[prev].0;
+                if ans[prev][prev_index] != 1_000_000_000 {
                     let arrival = itr + t;
-                    if node != nodes+1 {
-                        if arrival <= time_w[node].1 {
-                            let index = (arrival - time_w[node].0).max(0);
-                            ans[node][index] = ans[node][index]
-                                .min(ans[prev][itr - time_w[prev].0] + cost + (arrival - time_w[node].0).max(0) * node_costs[node]);
-                            println!("ans[{}][{}] = {}", node, prev, ans[node][index]);
+                    if arrival > time_w[node].1 {
+                        continue;
+                    }
+
+                    let index = (arrival as isize - time_w[node].0 as isize).max(0) as usize;
+                    let cost_here = ans[prev][prev_index] + cost + index * node_costs[node];
+
+                    if node != nodes + 1 {
+                        if cost_here < ans[node][index] {
+                            ans[node][index] = cost_here;
+                            parent[node][index] = Some((prev, prev_index));
                         }
-                    }  
-                    else { //sink node cost computation
-                        //println!("arrival: {} from {}", arrival, prev);
-                        res = res.min(ans[prev][itr - time_w[prev].0] + cost + (arrival - time_w[node].0).max(0) * node_costs[node])
-                    } 
+                    } else {
+                        if cost_here < res {
+                            res = cost_here;
+                            parent[node][index] = Some((prev, prev_index));
+                            final_arrival_time = index;
+                        }
+                    }
                 }
             }
         }
     }
 
     println!("{}", res);
+    // Reconstruct best path
+    let mut path = vec![];
+    let mut current_node = nodes+1;
+    let mut best_time_index = final_arrival_time;
+    if res == 1_000_000_000 {
+        println!("No valid path found.");
+        return;
+    }
+
+    while current_node != 0 {
+        path.push(current_node);
+        if let Some((prev, new_index)) = parent[current_node][best_time_index] {
+            best_time_index = new_index;
+            current_node = prev;
+        } else {
+            break;
+        }
+
+    }
+
+    path.push(0);
+    path.reverse();
+
+    println!("Best path: {:?}", path);
+
 }
