3b3c6d858788ee2b7bad434cf77d43557128c280
AndreiLeIttu <andrei.ionita2003@gmail.com>
2025-05-27 18:46:56 +0200

    Improve graph generation script to account for user input

--- testing/spptwtc/graphGenerator.py
+++ testing/spptwtc/graphGenerator.py
@@ -9,7 +9,7 @@ def generate_random_dag(num_nodes, edge_probability):
         for j in range(i + 1, num_nodes):
             if random.random() < edge_probability:
                 cost = random.randint(1, 10)  
-                time = random.randint(1, 10)
+                time = random.randint(1, 20)
                 G.add_edge(i, j, cost=cost, time=time)
     
     for node in G.nodes():
@@ -20,10 +20,13 @@ def generate_random_dag(num_nodes, edge_probability):
         G.nodes[node]['cost'] = cost if node != 0 else 0
     return G
 
-for i in range(10):
-    # generate 100 experiments and write them to a file
-    dag = generate_random_dag(20, 0.5)
-    with open(f"./testing/spptwtc/sparse_graph{i}.dzn", "w") as f:
+experiments_num = int(input("Enter the number of experiments to generate: "))
+graph_density = float(input("Enter the edge probability (between 0 and 1): "))
+
+for i in range(experiments_num):
+    # generate experiments and write them to a file
+    dag = generate_random_dag(70, graph_density)
+    with open(f"../dzn-inputs/dense_graph{i}.dzn", "w") as f:
         #number of nodes = N - 2, since the model expects the amount of non-terminal nodes
         nodesStr = "N={};\n".format(dag.number_of_nodes()-2)
         numEdgesStr = "M=" + str(dag.number_of_edges()) + ";\n"
@@ -45,3 +48,16 @@ for i in range(10):
             costsStr += f"{dag.nodes[node]['cost']},"
         costsStr += "];"
         f.write(nodesStr + numEdgesStr + edgesStr + windowsStr + costsStr)
+    with open(f"../rust-inputs/dense_graph{i}.in", "w") as f:
+        f.write(f"{dag.number_of_nodes() - 2} {dag.number_of_edges()}\n")
+        for u, v, data in dag.edges(data=True):
+            f.write(f"{u} {v} {data['time']} {data['cost']}\n")
+        for node in dag.nodes():
+            if node == 0:
+                continue
+            time_window = dag.nodes[node]['time_window']
+            f.write(f"{time_window[0]} {time_window[1]}\n")
+        for node in dag.nodes():
+            if node == 0:
+                continue
+            f.write(f"{dag.nodes[node]['cost']} ")
